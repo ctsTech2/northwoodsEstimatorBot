db29c7503ffc2605260f8bc207a2caca8dae5007
ctsTech2 <tempjuice@hotmail.com>
2024-01-09 14:58:08 -0600

    after completing basic portion, integrating thread management

--- assistants_quickstart.py
+++ assistants_quickstart.py
@@ -44,72 +44,87 @@ assistant = create_assistant(file)
 # --------------------------------------------------------------
 # Thread management
 # --------------------------------------------------------------
-# def check_if_thread_exists(wa_id):
-#     with shelve.open("threads_db") as threads_shelf:
-#         return threads_shelf.get(wa_id, None)
+def check_if_thread_exists(cust_id):
+    with shelve.open("threads_db") as threads_shelf:
+        return threads_shelf.get(cust_id, None)
 
 
-# def store_thread(wa_id, thread_id):
-#     with shelve.open("threads_db", writeback=True) as threads_shelf:
-#         threads_shelf[wa_id] = thread_id
+def store_thread(cust_id, thread_id):
+    with shelve.open("threads_db", writeback=True) as threads_shelf:
+        threads_shelf[cust_id] = thread_id
 
+# def generate_response(message_body):
+#     thread = client.beta.threads.create()
+#     thread_id = thread.id
 
-# --------------------------------------------------------------
-# Generate response
-# --------------------------------------------------------------
-# def generate_response(message_body, wa_id, name):
-#     # Check if there is already a thread_id for the wa_id
-#     thread_id = check_if_thread_exists(wa_id)
-
-#     # If a thread doesn't exist, create one and store it
-#     if thread_id is None:
-#         print(f"Creating new thread for {name} with wa_id {wa_id}")
-#         thread = client.beta.threads.create()
-#         store_thread(wa_id, thread.id)
-#         thread_id = thread.id
-
-#     # Otherwise, retrieve the existing thread
-#     else:
-#         print(f"Retrieving existing thread for {name} with wa_id {wa_id}")
-#         thread = client.beta.threads.retrieve(thread_id)
-
-#     # Add message to thread
 #     message = client.beta.threads.messages.create(
 #         thread_id=thread_id,
 #         role="user",
 #         content=message_body,
 #     )
+    
+#     message_body = "I'd like to get a quote for a 12x12 deck."
 
-#     # Run the assistant and get the new message
-#     new_message = run_assistant(thread)
-#     print(f"To {name}:", new_message)
-#     return new_message
+
+
+# --------------------------------------------------------------
+# Generate response
+# --------------------------------------------------------------
+def generate_response(message_body, cust_id, name):
+    # Check if there is already a thread_id for the cust_id
+    thread_id = check_if_thread_exists(cust_id)
+
+    # If a thread doesn't exist, create one and store it
+    # if thread_id is None:
+    #     print(f"Creating new thread for {name} with cust_id {cust_id}")
+    #     thread = client.beta.threads.create()
+    #     store_thread(cust_id, thread.id)
+    #     thread_id = thread.id
+
+    # # Otherwise, retrieve the existing thread
+    # else:
+    #     print(f"Retrieving existing thread for {name} with cust_id {cust_id}")
+    #     thread = client.beta.threads.retrieve(thread_id)
+
+    # # Add message to thread
+    # message = client.beta.threads.messages.create(
+    #     thread_id=thread_id,
+    #     role="user",
+    #     content=message_body,
+    # )
+
+    # # Run the assistant and get the new message
+    # new_message = run_assistant(thread)
+    # print(f"To {name}:", new_message)
+    # return new_message
 
 
 # # --------------------------------------------------------------
 # # Run assistant
 # # --------------------------------------------------------------
-# def run_assistant(thread):
-#     # Retrieve the Assistant
-#     assistant = client.beta.assistants.retrieve("asst_7Wx2nQwoPWSf710jrdWTDlfE")
+def run_assistant():
+    # Retrieve the Assistant
+    assistant = client.beta.assistants.retrieve("asst_bBnWdU3NCVf7lhZU27m1gwp1")
+    thread = client.beta.threads.retrieve("thread_ofejyOhWqHPM5aIeCYlSmo0p")
+    
 
 #     # Run the assistant
-#     run = client.beta.threads.runs.create(
-#         thread_id=thread.id,
-#         assistant_id=assistant.id,
-#     )
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
 
 #     # Wait for completion
-#     while run.status != "completed":
-#         # Be nice to the API
-#         time.sleep(0.5)
-#         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+    while run.status != "completed":
+        # Be nice to the API
+        time.sleep(0.5)
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
 
 #     # Retrieve the Messages
-#     messages = client.beta.threads.messages.list(thread_id=thread.id)
-#     new_message = messages.data[0].content[0].text.value
-#     print(f"Generated message: {new_message}")
-#     return new_message
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    new_message = messages.data[0].content[0].text.value
+    print(f"Generated message: {new_message}")
+    return new_message
 
 
 # # --------------------------------------------------------------
